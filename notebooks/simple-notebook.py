eb3c8d989f4fa77208841335435b06b3dd64bc50
luismiguelsaez <luismiguelsaez83@gmail.com>
2021-03-01 18:14:16 +0100

    Added PI calculation

--- notebooks/simple-notebook.py
+++ notebooks/simple-notebook.py
@@ -19,26 +19,16 @@ spark = SparkSession \
 
 sc = spark.sparkContext
 
-#conf = pyspark.SparkConf() \
-#    .setMaster('k8s://https://kubernetes.default.svc.cluster.local:443') \
-#    .set("spark.kubernetes.container.image", "spark-py:3.0.2") \
-#    .set("spark.kubernetes.namespace","jupyter") \
-#    .set("spark.kubernetes.authenticate.driver.serviceAccountName","jupyter") \
-#    .set("spark.kubernetes.authenticate.caCertFile", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt") \
-#    .set("spark.kubernetes.authenticate.oauthTokenFile", "/var/run/secrets/kubernetes.io/serviceaccount/token") \
-#    .set("spark.driverEnv.PYSPARK_PYTHON","python3") \
-#    .set("spark.executorEnv.PYSPARK_PYTHON","python3") \
-#    .set("spark.executor.instances", "2") \
-#    .set("spark.jars", "") \
-#    .set("spark.driver.host", "jupyter-all-spark-driver.jupyter.svc.cluster.local") \
-#    .set("spark.driver.port", "29413")
-#sc = pyspark.SparkContext(conf=conf)
+n = 100000 * 2
 
-rdd = sc.parallelize([1,2,3,4,5,6,7,8,9,10])
-rdd.collect()
-rdd.getNumPartitions()
+def f(_):
+    x = random() * 2 - 1
+    y = random() * 2 - 1
+    return 1 if x ** 2 + y ** 2 < 1 else 0
 
-#from pyspark.sql import SparkSession
-#
-#spark = SparkSession(sc)
-#df = spark.read.csv('/opt/spark/work-dir/dataset.csv')
+from operator import add
+
+count = sc.parallelize(range(1, n + 1), 2).map(f).reduce(add)
+print("Pi is roughly %f" % (4.0 * count / n))
+
+sc.stop()
