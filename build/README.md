4f6d9bdd9a20e2f99eb141f1f378fdce817baeca
luismiguelsaez <luismiguelsaez83@gmail.com>
2021-03-04 12:59:46 +0100

    Added fix for python2 not found error

--- build/README.md
+++ build/README.md
@@ -1,21 +1,21 @@
 
 ## Package download
 ```
-curl -sL https://ftp.cixug.es/apache/spark/spark-3.1.1/spark-3.1.1-bin-hadoop2.7.tgz -O
+curl -sL https://ftp.cixug.es/apache/spark/spark-3.0.2/spark-3.0.2-bin-hadoop2.7.tgz | gunzip | tar -xf -
 ```
 
 ## Current Jupyter image python version
 ```
-docker run --rm -it jupyter/all-spark-notebook:29edefbcb06a python -V
-Python 3.8.8
+docker run --rm -it jupyter/all-spark-notebook:29edefbcb06a python -V | sed 's/Python //g'
+3.8.8
 ```
 
 ## Build executor image with custom python bindings
 ```
-spark-3.1.1-bin-hadoop2.7/bin/docker-image-tool.sh -t 3.1.1-py3.8.8 -p bindings/python/Dockerfile -b java_image_tag=11-jre-slim -b python_version=3.8.8 build
+spark-3.0.2-bin-hadoop2.7/bin/docker-image-tool.sh -t 3.0.2-py3.8.8 -p bindings/python/Dockerfile -b java_image_tag=11-jre-slim -b python_version=3.8.8 build
 ```
 
 ## Add image to kind cluster
 ```
-kind load docker-image spark-py:3.1.1-py3.8.8 --name=jupyter-test
+kind load docker-image spark-py:3.0.2-py3.8.8 --name=jupyter-test
 ```
--- notebooks/simple-notebook.py
+++ notebooks/simple-notebook.py
@@ -5,13 +5,13 @@ spark = SparkSession \
     .appName("SparkSessionTest") \
     .master('k8s://https://kubernetes.default.svc.cluster.local:443') \
     .config("spark.submit.deployMode","client") \
-    .config("spark.kubernetes.container.image", "spark-py:3.0.2-py3-fix") \
+    .config("spark.kubernetes.container.image", "spark-py:3.0.2-py3.8.8") \
     .config("spark.kubernetes.namespace","jupyter") \
     .config("spark.kubernetes.authenticate.driver.serviceAccountName","jupyter") \
     .config("spark.kubernetes.authenticate.caCertFile", "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt") \
     .config("spark.kubernetes.authenticate.oauthTokenFile", "/var/run/secrets/kubernetes.io/serviceaccount/token") \
     .config("spark.executorEnv.PYSPARK_MAJOR_PYTHON_VERSION","3") \
-    .config("spark.executorEnv.PYSPARK_PYTHON","3") \
+    .config("spark.executorEnv.PYSPARK_PYTHON","/usr/bin/python3") \
     .config("spark.executor.instances", "2") \
     .config("spark.executor.extraClassPath", "") \
     .config("spark.executor.extraJavaOptions", "") \
@@ -23,16 +23,47 @@ spark = SparkSession \
 
 sc = spark.sparkContext
 
-n = 100000 * 2
+### PI calculation example
 
-def f(_):
-    x = random() * 2 - 1
-    y = random() * 2 - 1
-    return 1 if x ** 2 + y ** 2 < 1 else 0
+## Native
 
+import numpy as np
+from time import time
+from random import random
+
+inside = 0
+n = 10000000
+
+t_0 = time()
+for i in range(n):
+    x, y = random(), random()
+    if x**2 + y**2 < 1:
+        inside += 1
+print(np.round(time()-t_0, 3), "seconds elapsed for naive method and n=", n)
+print("pi is roughly", inside/n*4)
+
+## Spark job
+
+from time import time
+import numpy as np
+from random import random
 from operator import add
 
-count = sc.parallelize(range(1, n + 1), 2).map(f).reduce(add)
-print("Pi is roughly %f" % (4.0 * count / n))
+from pyspark.sql import SparkSession
 
-sc.stop()
+spark = SparkSession.builder.appName('CalculatePi').getOrCreate()
+sc = spark.sparkContext
+
+n = 10000000
+
+def is_point_inside_unit_circle(p):
+    # p is useless here
+    x, y = random(), random()
+    return 1 if x*x + y*y < 1 else 0
+
+t_0 = time()
+
+count = sc.parallelize(range(0, n)) \
+             .map(is_point_inside_unit_circle).reduce(add)
+print(np.round(time()-t_0, 3), "seconds elapsed for spark approach and n=", n)
+print("Pi is roughly %f" % (4.0 * count / n))
